89c5376af85fd38f1f02d31187bdbd0a3a90cde9
Yallow <tylerbouma1@gmail.com>
2015-05-02 16:03:14 -0500

    Fixed syntax errors

--- src/player.c
+++ src/player.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "dungeon.h"
-#include "dbg.h"
+#include "../dbg/dbg.h"
 
-player create_player(char *name, int health_points, int strength)
+player *create_player(char *name, int health_points, int strength)
 {
-  player new_player = calloc(1, sizeof(player));
+  player *new_player = calloc(1, sizeof(player));
   new_player->name = name;
   new_player->health_points = health_points;
   new_player->strength = strength;
@@ -13,9 +13,11 @@ player create_player(char *name, int health_points, int strength)
   printf("New player created: %s\n", new_player->name);
   printf("Hit points: %d\n", new_player->health_points);
   printf("Strength: %d\n", new_player->strength);
+
+  return new_player;
 }
 
-void player_move(move direction)
+void player_move(enum move direction)
 {
   switch(direction) {
   case MOVE_LEFT:
@@ -33,20 +35,20 @@ void player_move(move direction)
   default:
     printf("You can't move that direction\n");
     break;
+  }
 }
 
-bag create_bag()
+bag *create_bag()
 {
   bag *new_bag = calloc(1, sizeof(bag));
 
   return new_bag;
 }
 
-int main(int argc, char *argv[])
+int main()
 {
-  player tyler = create_player("Tyler", 50, 10);
-  void player_move(MOVE_LEFT);
+  player *tyler = create_player("Tyler", 50, 10);
+  void player_move(enum move MOVE_LEFT);
 
   return 0;
 }
-
